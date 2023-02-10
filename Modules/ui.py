e71e05289722e4771dfa7fc42b6a92c8a3872573
912-Dumitrana-Mihnea <mihnea.dumitrana@gmail.com>
2023-02-10 12:20:01 +0200

    Small aestethic changes to the console ui

--- Modules/ui.py
+++ Modules/ui.py
@@ -1,3 +1,4 @@
+import os
 from tkinter.filedialog import askdirectory
 import tkinter as tk
 import PrepareFile
@@ -6,6 +7,7 @@ import SortFolder
 class UI:
 
     def __init__(self):
+        os.system('cls')
         self.path = None
     
     def print_main(self):
@@ -36,20 +38,27 @@ class UI:
             option = self.get_input()
             if option == '1':
                 self.get_path()
+                os.system('cls')
             elif option == '2':
                 if self.path:
+                    os.system('cls')
                     PrepareFile.make_folders(self.path)
                     SortFolder.sort_files(self.path)
                 else:
+                    os.system('cls')
                     print('No folder selected!')
             elif option == '3':
                 if self.path:
+                    os.system('cls')
                     PrepareFile.make_folders(self.path)
                     SortFolder.sort_files(self.path)
                     PrepareFile.delete_empty_folders(self.path)
                 else:
+                    os.system('cls')
                     print('No folder selected!')
             elif option == '0':
                 exit()
             else:
+                os.system('cls')
                 print('Invalid option!')
+            
