915ce93a2be6665f0a6aa1924d971917ff214ad6
912-Dumitrana-Mihnea <mihnea.dumitrana@gmail.com>
2023-02-19 19:45:14 +0200

    Finished app with working GUI and Console UI

--- Modules/Print_Redirector.py
+++ Modules/Print_Redirector.py
@@ -9,5 +9,8 @@ class PrintRedirector:
         self.text_widget = text_widget
 
     def write(self, cmd):
+        if cmd.startswith("\033"):
+            cmd = cmd[5:-4]
+
         self.text_widget.insert(tk.END, cmd)
         self.text_widget.see(tk.END) # Scroll to the end of the console log
